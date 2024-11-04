bdfa8f1300f8bb049899ae3c973c8af19fbddb23
Hoa <h_cong@hetic.eu>
2024-11-04 16:34:32 +0100

    modification

--- Burger_icon.js
+++ Burger_icon.js
@@ -1,17 +1,9 @@
 window.onload = function () {
-	window.addEventListener('scroll', function (e) {
-		if (window.pageYOffset > 100) {
-			document.querySelector("header").classList.add('is-scrolling');
-		} else {
-			document.querySelector("header").classList.remove('is-scrolling');
-		}
-	});
-
 	const menu_btn = document.querySelector('.hamburger');
-	const mobile_menu = document.querySelector('.mobile-nav');
+	const mobile_menu = document.querySelector('.mobile_nav');
 
 	menu_btn.addEventListener('click', function () {
-		menu_btn.classList.toggle('is-active');
-		mobile_menu.classList.toggle('is-active');
+		menu_btn.classList.add('is_active');
+		mobile_menu.classList.toggle('is_active');
 	});
 }
